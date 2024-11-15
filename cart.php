cabbe5b46f9a1383d5e4b6cc5e800875f1307133
Gustavo Molina <hectorgustavomolinanevarez@gmail.com>
2024-11-15 15:50:11 -0600

    Add files via upload

--- cart.php
+++ cart.php
@@ -1,6 +1,10 @@
 <?php
-   $producto = $_GET['producto'];
-   $precio = $_GET['precio']; 
+  $producto = $_GET['producto'];
+  $precio = $_GET['precio'];
+
+  $file = @fopen("carritocompras.txt", "a"); 
+  fwrite($file, "$producto,$precio".PHP_EOL);
+  fclose($file); 
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -127,16 +131,23 @@
 						</tr>
 					</thead>
 					<tbody>
+						<?php 
+                            if(file_exists('carritocompras.txt')){
+                            $content = trim(file_get_contents('carritocompras.txt'), PHP_EOL);
+                            $lineas = explode(PHP_EOL, $content);
+                            foreach($lineas as $linea){
+                            list($productoE, $precioE) = explode(',', $linea);
+                         ?>
 						<tr>
 							<td class="cart_product">
 								<a href=""><img src="images/cart/one.png" alt=""></a>
 							</td>
 							<td class="cart_description">
-								<h4><a href=""><?php echo $producto; ?></a></h4>
+								<h4><a href=""><?php echo $productoE; ?></a></h4>
 								<p>Web ID: 1089772</p>
 							</td>
 							<td class="cart_price">
-								<p><?php echo $precio; ?></p>
+								<p><?php echo $precioE; ?></p>
 							</td>
 							<td class="cart_quantity">
 								<div class="cart_quantity_button">
@@ -146,12 +157,16 @@
 								</div>
 							</td>
 							<td class="cart_total">
-								<p class="cart_total_price"><?php echo $precio; ?></p>
+								<p class="cart_total_price"><?php echo $precioE; ?></p>
 							</td>
 							<td class="cart_delete">
 								<a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
 							</td>
 						</tr>
+						<?php
+						   }  //CIERRA EL SICLO FOR
+						 }    //CIERRA LA CONDICION IF
+						?>
 					</tbody>
 				</table>
 			</div>
@@ -173,7 +188,7 @@
 							<li>Total <span>$61</span></li>
 						</ul>
 							<a class="btn btn-default update" href="">Actualizar</a>
-							<a class="btn btn-default check_out" href="">Checar pedido</a>
+							<a class="btn btn-default check_out" href="vaciarcarrito.php">Vaciar Carrito</a>
 					</div>
 				</div>
 			</div>
